894d76e44cdd26478b09bcd1bf272bc4296b50c4
Jason Ouyang <jasonouyang112@gmail.com>
2021-04-10 14:57:51 -0700

    inital setup

---
+++ main.js
@@ -0,0 +1,7 @@
+const allSections = document.querySelectorAll('section');
+const searchSection = document.getElementById('section-search');
+const dashboardSection = document.getElementById('section-dashboard');
+const tradeSection = document.getElementById('section-trade');
+const allCryptosSection = document.getElementById('section-all-cryptos');
+
+
